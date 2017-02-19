84c31344f1e0e526bcbc1f4a9398a2d68bad44a1
kjng <pianistkevinjang@gmail.com>
2017-02-18 22:49:07 -0800

    Create api function for Google Stocks

---
+++ api.js
@@ -0,0 +1,19 @@
+const rootUrl = 'https://www.google.com/finance/info';
+
+export default (code) => {
+  var url = `${rootUrl}?q=${code}`;
+
+  return fetch(url)
+    .then(response => {
+      return response.text();
+    })
+    .then(text => {
+      console.log(text);
+      let json = JSON.parse(text.replace('//', '')[0]);
+      return {
+        stockIndex: json.l,
+        stockChangeRaw: json.c,
+        stockChangePercent: json.cp
+      }
+    });
+}
